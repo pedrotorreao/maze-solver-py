8a0b33c69698d7bc53087eda66a87174f3d9cf7b
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2023-07-06 22:08:03 -0300

    add comments to the cell class and clean up

--- cell.py
+++ cell.py
@@ -4,36 +4,45 @@ from line import Line
 
 
 class Cell:
+    # constructor:
     def __init__(self, wndw=None, p1=None, p2=None):
-        self._p1 = p1
-        self._p2 = p2
+        # cell coordinates in the canvas:
+        self._p1 = p1  # top-left corner
+        self._p2 = p2  # bottom-right corner
+        # window container:
         self._wndw = wndw
+        # by default, let us consider all the walls as existing:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
 
+    # draw the cell:
     def draw(self, fill_color):
         if self._wndw is None:
             return
+        # if the cell has a left wall, draw it:
         if self.has_left_wall:
             left_wall = Line(
                 Point(self._p1.x, self._p1.y), Point(self._p1.x, self._p2.y)
             )
             self._wndw.draw_line(left_wall, fill_color)
 
+        # if the cell has a right wall, draw it:
         if self.has_right_wall:
             right_wall = Line(
                 Point(self._p2.x, self._p1.y), Point(self._p2.x, self._p2.y)
             )
             self._wndw.draw_line(right_wall, fill_color)
 
+        # if the cell has a top wall, draw it:
         if self.has_top_wall:
             top_wall = Line(
                 Point(self._p1.x, self._p1.y), Point(self._p2.x, self._p1.y)
             )
             self._wndw.draw_line(top_wall, fill_color)
 
+        # if the cell has a bottom wall, draw it:
         if self.has_bottom_wall:
             bottom_wall = Line(
                 Point(self._p1.x, self._p2.y), Point(self._p2.x, self._p2.y)
@@ -41,9 +50,11 @@ class Cell:
             self._wndw.draw_line(bottom_wall, fill_color)
 
     def draw_move(self, to_cell, undo=False):
+        # calculate starting cell center:
         start_center_x = self._p1.x + float((self._p2.x - self._p1.x) / 2.0)
         start_center_y = self._p1.y + float((self._p2.y - self._p1.y) / 2.0)
 
+        # calculate ending cell center:
         end_center_x = to_cell._p1.x + float((to_cell._p2.x - to_cell._p1.x) / 2)
         end_center_y = to_cell._p1.y + float((to_cell._p2.y - to_cell._p1.y) / 2)
 
@@ -103,7 +114,3 @@ class Cell:
                 Point(end_center_x, to_cell._p1.y), Point(end_center_x, end_center_y)
             )
             self._wndw.draw_line(ln_2, fill_color)
-
-        # bridge_line = Line(coord_center_1, coord_center_2)
-
-        # self.__wndw.draw_line(bridge_line, fill_color)
